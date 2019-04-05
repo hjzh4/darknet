c7ff14f3e17aada4228d898170956df94d796a68
Hejia Zhang <hjzh578@gmail.com>
2019-04-05 02:55:02 -0700

    update darknet.py

--- python/darknet.py
+++ python/darknet.py
@@ -45,7 +45,7 @@ class METADATA(Structure):
     
 
 #lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("libdarknet.so", RTLD_GLOBAL)
+lib = CDLL("/home/hejia/Projects/darknet/libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
@@ -148,9 +148,10 @@ if __name__ == "__main__":
     #meta = load_meta("cfg/imagenet1k.data")
     #r = classify(net, meta, im)
     #print r[:10]
-    net = load_net("cfg/tiny-yolo.cfg", "tiny-yolo.weights", 0)
-    meta = load_meta("cfg/coco.data")
-    r = detect(net, meta, "data/dog.jpg")
-    print r
+    net = load_net(b"/home/hejia/Projects/darknet/sandbox/darkchef/minichef/yolov3.cfg",
+                   b"/home/hejia/Projects/darknet/sandbox/darkchef/minichef/backup/weights/yolov3_8000.weights", 0)
+    meta = load_meta(b"/home/hejia/Projects/darknet/sandbox/darkchef/minichef/trainer.data")
+    r = detect(net, meta, b"/home/hejia/Desktop/new/images/4871.jpg")
+    print(r)
     
 
